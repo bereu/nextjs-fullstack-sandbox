e5f0cfd7611aeda8576d720b7da98dc7f4af74f7
souhei.etou <souhei.etou@souheietounoMacBook-Pro.local>
2022-04-14 22:27:22 +0900

    initkl

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,19 @@ import React from "react"
 import { GetStaticProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
-
-export const getStaticProps: GetStaticProps = async () => {
-  const feed = [
-    {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-      published: false,
-      author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
-      },
-    },
-  ]
-  return { props: { feed } }
-}
+import prisma from "../lib/prisma";
 
 type Props = {
   feed: PostProps[]
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = await  prisma.post.findMany({
+    where: { published: true },
+  })
+  return { props: { feed } }
+}
+
 
 const Blog: React.FC<Props> = (props) => {
   return (
